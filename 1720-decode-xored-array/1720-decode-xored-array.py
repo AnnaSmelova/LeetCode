9f5e70cd6947ce0b7af734229585e8ab445f7a13
Anna Smelova <a_anna_a@inbox.ru>
2023-02-26 13:44:15 +0300

    Time: 225 ms (86.34%), Space: 15.9 MB (10.98%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.py
+++ 1720-decode-xored-array/1720-decode-xored-array.py
@@ -1,9 +1,7 @@
 class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:
-        n = len(encoded)
-        decoded = [0]*(n+1)
-        decoded[0] = first
-        for i in range(n):
-            decoded[i+1] = (encoded[i] ^ decoded[i])
-        return decoded
+        result=[first]
+        for i in encoded:
+            result.append(result[-1]^i)
+        return result
         
