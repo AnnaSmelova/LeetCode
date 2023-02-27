400ca0d00094c290657fe12262e3c07f176501ce
Anna Smelova <a_anna_a@inbox.ru>
2023-02-27 15:32:56 +0300

    Time: 21 ms (99.10%), Space: 13.9 MB (47.65%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,7 +1,3 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        sent = set(sentence)
-        alphabet = set(string.ascii_lowercase)
-        if sent == alphabet:
-            return True
-        return False
+        return len(set(sentence)) == 26
