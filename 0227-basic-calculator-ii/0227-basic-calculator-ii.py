f9c153db36d70f981308d35ece55f1e64abf0142
Anna Smelova <a_anna_a@inbox.ru>
2023-03-03 13:12:27 +0300

    Time: 99 ms (99.75%), Space: 17 MB (49.45%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,43 +1,24 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        arr = []
-        last_int = 0
-        for char in s:
-            if char.isnumeric():
-                if arr and last_int:
-                    last_char = arr.pop()
-                    last_char += char
-                    arr.append(last_char)
-                else:
-                    arr.append(char)
-                last_int = 1
-            elif char == ' ':
-                continue
-            else:
-                arr.append(char)
-                last_int = 0
+        current_number = 0
+        operation = '+'
+        s+='+'
         stack = []
-        i = 0
-        while i < len(arr):
-            if arr[i].isnumeric():
-                stack.append(arr[i])
-                i += 1
+        for char in s:
+            if char.isdigit():
+                current_number = current_number*10 + (ord(char)-ord('0'))
+            elif char == ' ':
+                    pass
             else:
-                if arr[i] == '*':
-                    last_num = int(stack.pop())
-                    res = last_num * int(arr[i + 1])
-                    stack.append(str(res))
-                elif arr[i] == '/':
-                    last_num = int(stack.pop())
-                    if last_num >= 0 and int(arr[i + 1]) >= 0:
-                        res = last_num // int(arr[i + 1])
-                    else:
-                        res = (-1) * (abs(last_num) // abs(int(arr[i + 1])))
-                    stack.append(str(res))
-                elif arr[i] == '+':
-                    stack.append(arr[i + 1])
-                else:
-                    stack.append('-' + arr[i + 1])
-                i += 2
-        return sum(list(map(int, stack)))
+                if operation == '+':
+                    stack.append(current_number)
+                elif operation == '-':
+                    stack.append(-current_number)
+                elif operation == '*':
+                    stack.append((stack.pop()*current_number))
+                elif operation == '/':
+                    stack.append(math.trunc(stack.pop()/current_number))
+                current_number = 0
+                operation = char
+        return sum(stack)
         
