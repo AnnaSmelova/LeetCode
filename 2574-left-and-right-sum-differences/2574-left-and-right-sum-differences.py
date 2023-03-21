a2c73de50a63324abe5e17a5db85674c01c93673
Anna Smelova <a_anna_a@inbox.ru>
2023-03-21 19:17:46 +0300

    Time: 75 ms (68.37%), Space: 14.2 MB (15.71%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -7,10 +7,10 @@ class Solution:
             j = n - i - 1
             left_sum[i] = left_sum[i - 1] + nums[i - 1]
             right_sum[i] = right_sum[i - 1] + nums[j + 1]
-        right_sum = right_sum[::-1]
+        #right_sum = right_sum[::-1]
         answer = [0] * n
         for i in range(n):
-            answer[i] = left_sum[i] - right_sum[i]
+            answer[i] = left_sum[i] - right_sum[n - i - 1]
             if answer[i] < 0:
                 answer[i] *= -1
         return answer
