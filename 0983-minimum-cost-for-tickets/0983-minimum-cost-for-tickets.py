b0c21063cd9a7f627abda44cf4b3ff3532c28a32
Anna Smelova <a_anna_a@inbox.ru>
2023-03-28 10:29:28 +0300

    Time: 59 ms (23.56%), Space: 15.1 MB (9.38%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+
+#dp(i)=min(dp(i+1)+costs[0],dp(i+7)+costs[1],dp(i+30)+costs[2])
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dayset = set(days)
+        durations = [1, 7, 30]
+
+        @lru_cache(None)
+        def dp(i):
+            if i > 365:
+                return 0
+            elif i in dayset:
+                return min(dp(i + d) + c
+                           for c, d in zip(costs, durations))
+            else:
+                return dp(i + 1)
+
+        return dp(1)
+        
